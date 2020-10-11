c7c111212de92c34d2c582aa90d46426152ccaf2
narenaruiz <narena@cifpfbmoll.eu>
2020-10-11 15:18:38 +0200

    Ejercicio 3 acabado

--- seccio1-nombres i strings/Ejercicio 3/ec03-exercicis.js
+++ seccio1-nombres i strings/Ejercicio 3/ec03-exercicis.js
@@ -1,4 +1,4 @@
-function comprobacion() {
+function quitarDecimales() {
     //capturar contingut de camp "input1"
 
     input1 = document.getElementById("input1").value;
@@ -6,12 +6,14 @@ function comprobacion() {
     console.log("El numero introduit es " + input1)
 
     if (input1 >= 0) {
-        document.getElementById("results").innerHTML = "Es un numero"
+        //Utilizo el Math.trunc(x) con el cual el numero que le pases te lo devolvera sin decimales.
+        document.getElementById("results").innerHTML = "El numero sin decimales es: " + Math.trunc(input1)
     } else {
         if (input1 < 0) {
-            document.getElementById("results").innerHTML = "Es un numero"
+            document.getElementById("results").innerHTML = "El numero sin decimales es: " + Math.trunc(input1)
         } else {
             document.getElementById("results").innerHTML = "No es un numero"
         }
     }
+
 }
